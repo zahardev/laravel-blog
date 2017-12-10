9e094be85b040b0e71bd1dccc76b511c6c742901
Sergey Zaharchenko <zaharchenko.dev@gmail.com>
2017-12-10 22:50:06 +0200

    Display posts from db

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //this is accessor method (starting with get and ending with Attribute)
+	public function getImageUrlAttribute($value) {
+		$imageUrl = '';
+		if(!is_null($this->image)){
+			$imagePath = public_path() . '/img/' . $this->image;
+			if(file_exists($imagePath)){
+				$imageUrl = asset('img/' . $this->image);
+			}
+		}
+		return $imageUrl;
+	}
 }
